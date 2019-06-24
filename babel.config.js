5c8c2df35604ba1a59716182026f17bba1ad33b0
Jiří Čermák <jiri.cermak@ackee.cz>
2019-06-24 09:11:05 +0200

    Configure babel-plugin-custom-import-path-transform

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,25 @@
+const path = require('path');
+
 module.exports = {
-    presets: ['@babel/typescript', '@babel/env', '@babel/react'],
-    plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
+    presets: [
+        '@babel/typescript',
+        [
+            '@babel/env',
+            {
+                modules: process.env.BABEL_ENV === 'es' ? false : 'auto',
+            },
+        ],
+        '@babel/react',
+    ],
+    plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        [
+            'babel-plugin-custom-import-path-transform',
+            {
+                transformImportPath: path.resolve(__dirname, 'scripts/transformImportPath.js'),
+            },
+        ],
+    ],
     ignore: ['**/__tests__/'],
 };
