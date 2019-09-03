f4ed9e613bc5ff8aba120904b876dae3f44fdddc
Lukas Horak <horaklukas@centrum.cz>
2019-09-03 10:33:30 +0200

    Refactor getting initial language in reducer

--- src/services/reducer.ts
+++ src/services/reducer.ts
@@ -3,12 +3,14 @@ import types from './actionTypes';
 
 import { isBrowserEnv } from '../config';
 
-const getInitialLanguage = (languages: string[]): string => {
+const getInitialLanguage = (languages: string | string[]): string => {
     let language = languages[0];
     const lang = isBrowserEnv ? window.navigator.language : language;
     const langSplitted = lang.split('-')[0];
 
-    if (languages.includes(lang)) {
+    if (typeof languages === 'string') {
+        language = languages;
+    } else if (languages.includes(lang)) {
         language = lang;
     } else if (languages.includes(langSplitted)) {
         language = langSplitted;
@@ -17,25 +19,23 @@ const getInitialLanguage = (languages: string[]): string => {
     return language;
 };
 
-const reducerFactory = (languages: string|string[]) => {
-    const language = Array.isArray(languages) ? getInitialLanguage(languages) : languages;
-    const initialState: LocaleState = {
-        locale: language,
-    };
-
-    return (state: LocaleState = initialState, action: Action) => {
-        switch (action.type) {
-            case types.SET_LOCALE:
-                return {
-                    ...state,
-                    locale: action.locale,
-                };
-            case types.GET_LOCALE:
-                return state;
-            default:
-                return state;
-        }
-    };
+const reducerFactory = (languages: string | string[]) => (
+    state: LocaleState = {
+        locale: getInitialLanguage(languages),
+    },
+    action: Action,
+) => {
+    switch (action.type) {
+        case types.SET_LOCALE:
+            return {
+                ...state,
+                locale: action.locale,
+            };
+        case types.GET_LOCALE:
+            return state;
+        default:
+            return state;
+    }
 };
 
 export default reducerFactory;
